c073adf3b4c2f7fa2fb16c48c2eef741fb8de56e
PV <PV@DESKTOP-FO6MTI0>
2025-02-23 19:40:36 +0700

    Add project files.

---
+++ KTLT B10.1/Form1.Designer.cs
@@ -0,0 +1,263 @@
+﻿namespace KTLT_B10._1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.rButtonUC = new System.Windows.Forms.RadioButton();
+            this.rButtonBC = new System.Windows.Forms.RadioButton();
+            this.buttonExit = new System.Windows.Forms.Button();
+            this.buttonKQ = new System.Windows.Forms.Button();
+            this.buttonSkip = new System.Windows.Forms.Button();
+            this.textBoxKQ = new System.Windows.Forms.TextBox();
+            this.rButtonPTB1 = new System.Windows.Forms.RadioButton();
+            this.rButtonPTB2 = new System.Windows.Forms.RadioButton();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(36, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(100, 23);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Nhap";
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(36, 61);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(100, 23);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "A";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 89);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(100, 23);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "B";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(502, 26);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(128, 23);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Chon tim";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(36, 168);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(100, 23);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Ket qua";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(109, 62);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(346, 22);
+            this.textBox1.TabIndex = 5;
+            this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(109, 90);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(346, 22);
+            this.textBox2.TabIndex = 6;
+            // 
+            // rButtonUC
+            // 
+            this.rButtonUC.AutoSize = true;
+            this.rButtonUC.Location = new System.Drawing.Point(507, 61);
+            this.rButtonUC.Name = "rButtonUC";
+            this.rButtonUC.Size = new System.Drawing.Size(64, 20);
+            this.rButtonUC.TabIndex = 7;
+            this.rButtonUC.TabStop = true;
+            this.rButtonUC.Text = "UCLN";
+            this.rButtonUC.UseVisualStyleBackColor = true;
+            // 
+            // rButtonBC
+            // 
+            this.rButtonBC.AutoSize = true;
+            this.rButtonBC.Location = new System.Drawing.Point(507, 88);
+            this.rButtonBC.Name = "rButtonBC";
+            this.rButtonBC.Size = new System.Drawing.Size(66, 20);
+            this.rButtonBC.TabIndex = 8;
+            this.rButtonBC.TabStop = true;
+            this.rButtonBC.Text = "BCNN";
+            this.rButtonBC.UseVisualStyleBackColor = true;
+            // 
+            // buttonExit
+            // 
+            this.buttonExit.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonExit.Location = new System.Drawing.Point(475, 256);
+            this.buttonExit.Name = "buttonExit";
+            this.buttonExit.Size = new System.Drawing.Size(135, 49);
+            this.buttonExit.TabIndex = 10;
+            this.buttonExit.Text = "Thoat";
+            this.buttonExit.UseVisualStyleBackColor = true;
+            this.buttonExit.Click += new System.EventHandler(this.buttonExit_Click);
+            // 
+            // buttonKQ
+            // 
+            this.buttonKQ.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonKQ.Location = new System.Drawing.Point(41, 256);
+            this.buttonKQ.Name = "buttonKQ";
+            this.buttonKQ.Size = new System.Drawing.Size(135, 49);
+            this.buttonKQ.TabIndex = 11;
+            this.buttonKQ.Text = "Tim";
+            this.buttonKQ.UseVisualStyleBackColor = true;
+            this.buttonKQ.Click += new System.EventHandler(this.buttonKQ_Click);
+            // 
+            // buttonSkip
+            // 
+            this.buttonSkip.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonSkip.Location = new System.Drawing.Point(259, 256);
+            this.buttonSkip.Name = "buttonSkip";
+            this.buttonSkip.Size = new System.Drawing.Size(135, 49);
+            this.buttonSkip.TabIndex = 12;
+            this.buttonSkip.Text = "Bo qua";
+            this.buttonSkip.UseVisualStyleBackColor = true;
+            this.buttonSkip.Click += new System.EventHandler(this.buttonSkip_Click);
+            // 
+            // textBoxKQ
+            // 
+            this.textBoxKQ.BackColor = System.Drawing.SystemColors.Info;
+            this.textBoxKQ.Location = new System.Drawing.Point(109, 203);
+            this.textBoxKQ.Name = "textBoxKQ";
+            this.textBoxKQ.ReadOnly = true;
+            this.textBoxKQ.Size = new System.Drawing.Size(346, 22);
+            this.textBoxKQ.TabIndex = 13;
+            // 
+            // rButtonPTB1
+            // 
+            this.rButtonPTB1.AutoSize = true;
+            this.rButtonPTB1.Location = new System.Drawing.Point(507, 115);
+            this.rButtonPTB1.Name = "rButtonPTB1";
+            this.rButtonPTB1.Size = new System.Drawing.Size(82, 20);
+            this.rButtonPTB1.TabIndex = 14;
+            this.rButtonPTB1.TabStop = true;
+            this.rButtonPTB1.Text = "PT bac 1";
+            this.rButtonPTB1.UseVisualStyleBackColor = true;
+            // 
+            // rButtonPTB2
+            // 
+            this.rButtonPTB2.AutoSize = true;
+            this.rButtonPTB2.Location = new System.Drawing.Point(507, 142);
+            this.rButtonPTB2.Name = "rButtonPTB2";
+            this.rButtonPTB2.Size = new System.Drawing.Size(82, 20);
+            this.rButtonPTB2.TabIndex = 15;
+            this.rButtonPTB2.TabStop = true;
+            this.rButtonPTB2.Text = "PT bac 2";
+            this.rButtonPTB2.UseVisualStyleBackColor = true;
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Times New Roman", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(36, 115);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(100, 23);
+            this.label6.TabIndex = 16;
+            this.label6.Text = "C";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(109, 118);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(346, 22);
+            this.textBox3.TabIndex = 17;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.ClientSize = new System.Drawing.Size(701, 351);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.rButtonPTB2);
+            this.Controls.Add(this.rButtonPTB1);
+            this.Controls.Add(this.textBoxKQ);
+            this.Controls.Add(this.buttonSkip);
+            this.Controls.Add(this.buttonKQ);
+            this.Controls.Add(this.buttonExit);
+            this.Controls.Add(this.rButtonBC);
+            this.Controls.Add(this.rButtonUC);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.RadioButton rButtonUC;
+        private System.Windows.Forms.RadioButton rButtonBC;
+        private System.Windows.Forms.Button buttonExit;
+        private System.Windows.Forms.Button buttonKQ;
+        private System.Windows.Forms.Button buttonSkip;
+        private System.Windows.Forms.TextBox textBoxKQ;
+        private System.Windows.Forms.RadioButton rButtonPTB1;
+        private System.Windows.Forms.RadioButton rButtonPTB2;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox3;
+    }
+}
+
---
+++ KTLT B10.1/Form1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Text;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace KTLT_B10._1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            textBoxKQ.Text=textBox1.Text;
+        }
+
+        private void buttonExit_Click(object sender, EventArgs e)
+        {
+            DialogResult o = MessageBox.Show("Ban muon thoat ko?", "Xac nhan", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
+            if (o == DialogResult.Yes)
+                this.Close();
+        }
+
+        private void buttonKQ_Click(object sender, EventArgs e)
+        {
+            if (rButtonUC.Checked)
+            {
+
+            }
+        }
+
+        private void buttonSkip_Click(object sender, EventArgs e)
+        {
+            textBox1.Text = " ";
+            textBox2.Text = " ";
+            textBox3.Text = " ";
+            textBoxKQ.Text = " ";
+        }
+        private int n;
+        private int m;
+        string UC()
+        {
+
+        }
+        void UCLN() 
+        {
+        
+        }
+    }
+}
