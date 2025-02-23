2215dc76a1635a9cf9412a5e0d0975d4b3a45964
AmadeusIV <quanhm0412@gmail.com>
2025-02-23 21:00:08 +0700

    V.Final.

--- KTLT B10.1/Form1.Designer.cs
+++ KTLT B10.1/Form1.Designer.cs
@@ -52,77 +52,77 @@
             // 
             this.label1.BackColor = System.Drawing.Color.Transparent;
             this.label1.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.label1.Location = new System.Drawing.Point(32, 24);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(43, 30);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(75, 27);
+            this.label1.Size = new System.Drawing.Size(100, 33);
             this.label1.TabIndex = 0;
             this.label1.Text = "Nhập";
             // 
             // label5
             // 
             this.label5.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.label5.Location = new System.Drawing.Point(37, 191);
-            this.label5.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label5.Location = new System.Drawing.Point(49, 235);
             this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(75, 24);
+            this.label5.Size = new System.Drawing.Size(100, 30);
             this.label5.TabIndex = 4;
             this.label5.Text = "Kết quả";
             // 
             // textBox1
             // 
             this.textBox1.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.textBox1.Location = new System.Drawing.Point(81, 68);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox1.Location = new System.Drawing.Point(108, 84);
+            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(260, 28);
+            this.textBox1.Size = new System.Drawing.Size(345, 33);
             this.textBox1.TabIndex = 5;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
             // 
             // textBox2
             // 
             this.textBox2.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.textBox2.Location = new System.Drawing.Point(81, 102);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox2.Location = new System.Drawing.Point(108, 126);
+            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(260, 28);
+            this.textBox2.Size = new System.Drawing.Size(345, 33);
             this.textBox2.TabIndex = 6;
             // 
             // rButtonUC
             // 
             this.rButtonUC.AutoSize = true;
             this.rButtonUC.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.rButtonUC.Location = new System.Drawing.Point(9, 26);
-            this.rButtonUC.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rButtonUC.Location = new System.Drawing.Point(12, 32);
+            this.rButtonUC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rButtonUC.Name = "rButtonUC";
-            this.rButtonUC.Size = new System.Drawing.Size(72, 25);
+            this.rButtonUC.Size = new System.Drawing.Size(88, 30);
             this.rButtonUC.TabIndex = 7;
             this.rButtonUC.TabStop = true;
             this.rButtonUC.Text = "UCLN";
             this.rButtonUC.UseVisualStyleBackColor = true;
+            this.rButtonUC.CheckedChanged += new System.EventHandler(this.rButtonUC_CheckedChanged);
             // 
             // rButtonBC
             // 
             this.rButtonBC.AutoSize = true;
             this.rButtonBC.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.rButtonBC.Location = new System.Drawing.Point(9, 55);
-            this.rButtonBC.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rButtonBC.Location = new System.Drawing.Point(12, 68);
+            this.rButtonBC.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rButtonBC.Name = "rButtonBC";
-            this.rButtonBC.Size = new System.Drawing.Size(74, 25);
+            this.rButtonBC.Size = new System.Drawing.Size(90, 30);
             this.rButtonBC.TabIndex = 8;
             this.rButtonBC.TabStop = true;
             this.rButtonBC.Text = "BCNN";
             this.rButtonBC.UseVisualStyleBackColor = true;
+            this.rButtonBC.CheckedChanged += new System.EventHandler(this.rButtonBC_CheckedChanged);
             // 
             // buttonExit
             // 
             this.buttonExit.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
             this.buttonExit.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
             this.buttonExit.ForeColor = System.Drawing.Color.White;
-            this.buttonExit.Location = new System.Drawing.Point(425, 248);
-            this.buttonExit.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonExit.Location = new System.Drawing.Point(567, 305);
+            this.buttonExit.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonExit.Name = "buttonExit";
-            this.buttonExit.Size = new System.Drawing.Size(101, 40);
+            this.buttonExit.Size = new System.Drawing.Size(135, 49);
             this.buttonExit.TabIndex = 10;
             this.buttonExit.Text = "Thoát";
             this.buttonExit.UseVisualStyleBackColor = false;
@@ -133,10 +133,10 @@
             this.buttonKQ.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
             this.buttonKQ.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
             this.buttonKQ.ForeColor = System.Drawing.Color.White;
-            this.buttonKQ.Location = new System.Drawing.Point(76, 248);
-            this.buttonKQ.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.buttonKQ.Location = new System.Drawing.Point(101, 305);
+            this.buttonKQ.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.buttonKQ.Name = "buttonKQ";
-            this.buttonKQ.Size = new System.Drawing.Size(101, 40);
+            this.buttonKQ.Size = new System.Drawing.Size(135, 49);
             this.buttonKQ.TabIndex = 11;
             this.buttonKQ.Text = "Tìm";
             this.buttonKQ.UseVisualStyleBackColor = false;
@@ -144,11 +144,13 @@
             // 
             // buttonSkip
             // 
+            this.buttonSkip.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
             this.buttonSkip.Font = new System.Drawing.Font("Times New Roman", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.buttonSkip.Location = new System.Drawing.Point(259, 256);
-            this.buttonSkip.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
+            this.buttonSkip.ForeColor = System.Drawing.Color.White;
+            this.buttonSkip.Location = new System.Drawing.Point(335, 305);
+            this.buttonSkip.Margin = new System.Windows.Forms.Padding(4, 2, 4, 2);
             this.buttonSkip.Name = "buttonSkip";
-            this.buttonSkip.Size = new System.Drawing.Size(101, 40);
+            this.buttonSkip.Size = new System.Drawing.Size(135, 49);
             this.buttonSkip.TabIndex = 12;
             this.buttonSkip.Text = "Bỏ Qua";
             this.buttonSkip.UseVisualStyleBackColor = false;
@@ -159,11 +161,11 @@
             this.textBoxKQ.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(56)))), ((int)(((byte)(56)))), ((int)(((byte)(56)))));
             this.textBoxKQ.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
             this.textBoxKQ.ForeColor = System.Drawing.Color.White;
-            this.textBoxKQ.Location = new System.Drawing.Point(116, 188);
-            this.textBoxKQ.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBoxKQ.Location = new System.Drawing.Point(155, 231);
+            this.textBoxKQ.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBoxKQ.Name = "textBoxKQ";
             this.textBoxKQ.ReadOnly = true;
-            this.textBoxKQ.Size = new System.Drawing.Size(260, 28);
+            this.textBoxKQ.Size = new System.Drawing.Size(345, 33);
             this.textBoxKQ.TabIndex = 13;
             this.textBoxKQ.TextChanged += new System.EventHandler(this.textBoxKQ_TextChanged);
             // 
@@ -171,36 +173,37 @@
             // 
             this.rButtonPTB1.AutoSize = true;
             this.rButtonPTB1.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.rButtonPTB1.Location = new System.Drawing.Point(9, 84);
-            this.rButtonPTB1.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rButtonPTB1.Location = new System.Drawing.Point(12, 103);
+            this.rButtonPTB1.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rButtonPTB1.Name = "rButtonPTB1";
-            this.rButtonPTB1.Size = new System.Drawing.Size(99, 25);
+            this.rButtonPTB1.Size = new System.Drawing.Size(119, 30);
             this.rButtonPTB1.TabIndex = 14;
             this.rButtonPTB1.TabStop = true;
             this.rButtonPTB1.Text = "PT bậc 1";
             this.rButtonPTB1.UseVisualStyleBackColor = true;
+            this.rButtonPTB1.CheckedChanged += new System.EventHandler(this.rButtonPTB1_CheckedChanged);
             // 
             // rButtonPTB2
             // 
             this.rButtonPTB2.AutoSize = true;
             this.rButtonPTB2.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.rButtonPTB2.Location = new System.Drawing.Point(9, 113);
-            this.rButtonPTB2.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.rButtonPTB2.Location = new System.Drawing.Point(12, 139);
+            this.rButtonPTB2.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.rButtonPTB2.Name = "rButtonPTB2";
-            this.rButtonPTB2.Size = new System.Drawing.Size(99, 25);
+            this.rButtonPTB2.Size = new System.Drawing.Size(119, 30);
             this.rButtonPTB2.TabIndex = 15;
             this.rButtonPTB2.TabStop = true;
             this.rButtonPTB2.Text = "PT bậc 2";
             this.rButtonPTB2.UseVisualStyleBackColor = true;
+            this.rButtonPTB2.CheckedChanged += new System.EventHandler(this.rButtonPTB2_CheckedChanged);
             // 
             // label6
             // 
             this.label6.BackColor = System.Drawing.Color.Transparent;
             this.label6.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.label6.Location = new System.Drawing.Point(51, 140);
-            this.label6.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label6.Location = new System.Drawing.Point(68, 172);
             this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(21, 23);
+            this.label6.Size = new System.Drawing.Size(28, 28);
             this.label6.TabIndex = 16;
             this.label6.Text = "C";
             this.label6.Click += new System.EventHandler(this.label6_Click);
@@ -208,10 +211,10 @@
             // textBox3
             // 
             this.textBox3.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.textBox3.Location = new System.Drawing.Point(81, 137);
-            this.textBox3.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.textBox3.Location = new System.Drawing.Point(108, 169);
+            this.textBox3.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.textBox3.Name = "textBox3";
-            this.textBox3.Size = new System.Drawing.Size(260, 28);
+            this.textBox3.Size = new System.Drawing.Size(345, 33);
             this.textBox3.TabIndex = 17;
             // 
             // groupBox1
@@ -221,9 +224,11 @@
             this.groupBox1.Controls.Add(this.rButtonPTB2);
             this.groupBox1.Controls.Add(this.rButtonPTB1);
             this.groupBox1.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.groupBox1.Location = new System.Drawing.Point(397, 39);
+            this.groupBox1.Location = new System.Drawing.Point(529, 48);
+            this.groupBox1.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.groupBox1.Name = "groupBox1";
-            this.groupBox1.Size = new System.Drawing.Size(129, 149);
+            this.groupBox1.Padding = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.groupBox1.Size = new System.Drawing.Size(172, 183);
             this.groupBox1.TabIndex = 18;
             this.groupBox1.TabStop = false;
             this.groupBox1.Text = "Chọn";
@@ -232,10 +237,9 @@
             // 
             this.label2.BackColor = System.Drawing.Color.Transparent;
             this.label2.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.label2.Location = new System.Drawing.Point(51, 71);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(68, 87);
             this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(21, 19);
+            this.label2.Size = new System.Drawing.Size(28, 23);
             this.label2.TabIndex = 1;
             this.label2.Text = "A";
             // 
@@ -243,19 +247,18 @@
             // 
             this.label3.BackColor = System.Drawing.Color.Transparent;
             this.label3.Font = new System.Drawing.Font("Cambria", 13F, System.Drawing.FontStyle.Bold);
-            this.label3.Location = new System.Drawing.Point(51, 105);
-            this.label3.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label3.Location = new System.Drawing.Point(68, 129);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(21, 19);
+            this.label3.Size = new System.Drawing.Size(28, 23);
             this.label3.TabIndex = 2;
             this.label3.Text = "B";
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(214)))), ((int)(((byte)(214)))), ((int)(((byte)(214)))));
-            this.ClientSize = new System.Drawing.Size(588, 312);
+            this.ClientSize = new System.Drawing.Size(784, 384);
             this.Controls.Add(this.groupBox1);
             this.Controls.Add(this.textBox3);
             this.Controls.Add(this.label6);
@@ -269,8 +272,8 @@
             this.Controls.Add(this.label3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
-            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Margin = new System.Windows.Forms.Padding(3, 2, 3, 2);
             this.Name = "Form1";
             this.Text = "Form1";
             this.groupBox1.ResumeLayout(false);
--- KTLT B10.1/Form1.cs
+++ KTLT B10.1/Form1.cs
@@ -18,18 +18,14 @@ namespace KTLT_B10._1
             InitializeComponent();
         }
 
-        private void textBox1_TextChanged(object sender, EventArgs e)
-        {
-            textBoxKQ.Text=textBox1.Text;
-        }
-
+        private void textBox1_TextChanged(object sender, EventArgs e) { }
         private void buttonExit_Click(object sender, EventArgs e)
         {
             DialogResult o = MessageBox.Show("Ban muon thoat ko?", "Xac nhan", MessageBoxButtons.YesNo, MessageBoxIcon.Question);
             if (o == DialogResult.Yes)
                 this.Close();
         }
-
+        
         private void buttonKQ_Click(object sender, EventArgs e)
         {
             if (rButtonUC.Checked)
@@ -44,6 +40,73 @@ namespace KTLT_B10._1
                     MessageBox.Show("Vui long nhap so nguyen hop le", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
                 }
             }
+            else if (rButtonBC.Checked)
+            {
+                if (int.TryParse(textBox1.Text, out int a) && int.TryParse(textBox2.Text, out int b) && int.TryParse(textBox3.Text, out int c))
+                {
+                    long temp1 = (long)a * b / UCLN(a, b);
+                    long result = (temp1 * c) / UCLN((int)temp1, c);
+                    textBoxKQ.Text = result.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Vui long nhap so nguyen hop le", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if (rButtonPTB1.Checked)
+            {
+                
+                if (int.TryParse(textBox1.Text, out int a) && int.TryParse(textBox2.Text, out int b))
+                {
+                    if (a == 0 && b == 0)
+                    {
+                        textBoxKQ.Text = "Phuong trinh vo so nghiem";
+                    }
+                    else if (a == 0 && b != 0)
+                    {
+                        textBoxKQ.Text = "Phuong trinh vo nghiem";
+                    }
+                    else
+                    {
+                        double result = (double)-b / a;
+                        textBoxKQ.Text = result.ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vui long nhap so nguyen hop le", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else if(rButtonPTB2.Checked)
+            {
+                if (int.TryParse(textBox1.Text, out int a) && int.TryParse(textBox2.Text, out int b) && int.TryParse(textBox3.Text, out int c))
+                {
+                    double delta = b * b - 4 * a * c;
+                    if (delta < 0)
+                    {
+                        textBoxKQ.Text = "Phuong trinh vo nghiem";
+                    }
+                    else if (delta == 0)
+                    {
+                        double result = (double)-b / (2 * a);
+                        textBoxKQ.Text = result.ToString();
+                    }
+                    else
+                    {
+                        double result1 = (-b + Math.Sqrt(delta)) / (2 * a);
+                        double result2 = (-b - Math.Sqrt(delta)) / (2 * a);
+                        textBoxKQ.Text = "x1 = " + result1.ToString() + " x2 = " + result2.ToString();
+                    }
+                }
+                else
+                {
+                    MessageBox.Show("Vui long nhap so nguyen hop le", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+            else
+            {
+                MessageBox.Show("Vui long chon 1 trong 4 chuc nang", "Loi", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
         }
 
         private void buttonSkip_Click(object sender, EventArgs e)
@@ -55,7 +118,7 @@ namespace KTLT_B10._1
         }
         private int UCLN(int a, int b)
         {
-            while (a != 0)
+            while (b != 0)
             {
                 int temp = b;
                 b = a % b;
@@ -63,19 +126,34 @@ namespace KTLT_B10._1
             }
             return a;
         }
-        private string UC()
+        
+
+        private void label6_Click(object sender, EventArgs e) { }
+
+        private void textBoxKQ_TextChanged(object sender, EventArgs e) { }
+
+        private void rButtonPTB1_CheckedChanged(object sender, EventArgs e)
         {
-            return "";
+            label6.Visible = false;
+            textBox3.Visible = false;
         }
 
-        private void label6_Click(object sender, EventArgs e)
+        private void rButtonUC_CheckedChanged(object sender, EventArgs e)
         {
-
+            label6.Visible = true;
+            textBox3.Visible = true;
         }
 
-        private void textBoxKQ_TextChanged(object sender, EventArgs e)
+        private void rButtonBC_CheckedChanged(object sender, EventArgs e)
         {
+            label6.Visible = true;
+            textBox3.Visible = true;
+        }
 
+        private void rButtonPTB2_CheckedChanged(object sender, EventArgs e)
+        {
+            label6.Visible = true;
+            textBox3.Visible = true;
         }
     }
 }
